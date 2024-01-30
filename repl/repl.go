fbb74aeab9dad880e89afaec01620af5602fc1dd
ntdkhang <ntdkhang143@gmail.com>
2024-01-30 17:16:53 -0600

    fully running repl

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import(
     "fmt"
     "io"
     "A-Plus-Plus/lexer"
-    "A-Plus-Plus/token"
+    "A-Plus-Plus/parser"
 )
 
 const PROMPT = ">> "
@@ -21,9 +21,22 @@ func Start(in io.Reader, out io.Writer) {
         }
         line := scanner.Text()
         l := lexer.New(line)
-
-        for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-            fmt.Fprintf(out, "%+v\n", tok)
+        p := parser.New(l)
+        program := p.ParseProgram()
+        if len(p.Errors()) != 0 {
+            printParserErrors(out, p.Errors())
+            continue
         }
+
+        io.WriteString(out, program.String())
+        io.WriteString(out, "\n")
+
     }
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+    for _, msg := range errors {
+        io.WriteString(out, "\t" + msg + "\n")
+    }
+
 }
