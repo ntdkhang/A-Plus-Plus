b7ac93e83e0fc4ac7a35219405fad6883c7f5e5a
ntdkhang <ntdkhang143@gmail.com>
2024-01-30 13:44:03 -0600

    parse function literal

--- AST/ast.go
+++ AST/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
     "A-Plus-Plus/token"
     "bytes"
+    "strings"
 )
 
 type Node interface {
@@ -222,3 +223,29 @@ func (bs *BlockStatement) String() string {
     return out.String()
 }
 
+
+
+type FunctionLiteral struct {
+    Token       token.Token // the 'fn' token
+    Parameters  []*Identifier
+    Body        *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() { }
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+    var out bytes.Buffer
+
+    params := []string{}
+    for _, p := range fl.Parameters {
+        params = append(params,p.String())
+    }
+
+    out.WriteString("fn")
+    out.WriteString("(")
+    out.WriteString(strings.Join(params, ", "))
+    out.WriteString(") ")
+    out.WriteString(fl.Body.String())
+
+    return out.String()
+}
