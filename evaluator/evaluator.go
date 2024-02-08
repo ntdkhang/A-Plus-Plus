5f226b5a4a1024583bfcc7a5afd44a5e0b79f7d4
ntdkhang <ntdkhang143@gmail.com>
2024-02-08 11:32:12 -0600

    environment scoping

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -16,18 +16,23 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
     switch node := node.(type) {
     case *ast.Program:
         return evalProgram(node.Statements, env)
+
     case *ast.ExpressionStatement:
         return Eval(node.Expression, env)
+
     case *ast.IntegerLiteral:
         return &object.Integer{Value: node.Value}
+
     case *ast.Boolean:
         return nativeBoolToBooleanObject(node.Value)
+
     case *ast.PrefixExpression:
         right := Eval(node.Right, env)
         if isError(right) {
             return right
         }
         return evalPrefixExpression(node.Operator, right, env)
+
     case *ast.InfixExpression:
         left := Eval(node.Left, env)
         if isError(left) {
@@ -38,30 +43,60 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
             return right
         }
         return evalInfixExpression(node.Operator, left, right, env)
+
     case *ast.IfExpression:
         return evalIfExpression(node, env)
+
     case *ast.ReturnStatement:
         val := Eval(node.ReturnValue, env)
         if isError(val) {
             return val
         }
         return &object.ReturnValue{Value: val}
+
     case *ast.BlockStatement:
         return evalBlockStatement(node, env)
+
     case *ast.LetStatement:
         val := Eval(node.Value, env)
         if isError(val) {
             return val
         }
         env.Set(node.Name.Value, val)
+
     case *ast.Identifier:
         return evalIdentifier(node, env)
+
     case *ast.FunctionLiteral:
         params := node.Parameters
         body := node.Body
         return &object.Function{Parameters: params, Env: env, Body: body}
+
+    case *ast.CallExpression:
+        function := Eval(node.Function, env)
+        if isError(function) {
+            return function
+        }
+        args := evalExpressions(node.Arguments, env)
+        if len(args) == 1 && isError(args[0]) {
+            return args[0]
+        }
+        return applyFunction(function, args)
     }
     return nil
+}
+
+
+func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
+    var result []object.Object
+    for _, e := range exps {
+        evaluated := Eval(e, env)
+        if isError(evaluated) {
+            return []object.Object{evaluated}
+        }
+        result = append(result, evaluated)
+    }
+    return result
 }
 
 
@@ -231,4 +266,32 @@ func isError(obj object.Object) bool {
         return obj.Type() == object.ERROR_OBJ
     }
     return false
+}
+
+
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+    function, ok := fn.(*object.Function)
+    if !ok {
+        return newError("not a function: %s", fn.Type())
+    }
+    extendedEnv := extendFunctionEnv(function, args)
+    evaluated := Eval(function.Body, extendedEnv)
+    return unwrapReturnValue(evaluated)
+}
+
+
+func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
+    env := object.NewEnclosedEnvironment(fn.Env)
+    for paramIdx, param := range fn.Parameters {
+        env.Set(param.Value, args[paramIdx])
+    }
+    return env
+}
+
+
+func unwrapReturnValue(obj object.Object) object.Object {
+    if returnValue, ok := obj.(*object.ReturnValue); ok {
+        return returnValue.Value
+    }
+    return obj
 }
