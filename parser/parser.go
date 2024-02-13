7055eb568dd5a80b5a4367a8029b7381a60f5271
ntdkhang <ntdkhang143@gmail.com>
2024-02-13 11:10:09 -0600

    parse index operator

--- parser/parser.go
+++ parser/parser.go
@@ -69,7 +69,18 @@ func New(l *lexer.Lexer) *Parser {
     p.registerInfix(token.LT, p.parseInfixExpression)
     p.registerInfix(token.GT, p.parseInfixExpression)
     p.registerInfix(token.LPAREN, p.parseCallExpression)
+    p.registerInfix(token.LBRACKET, p.parseIndexExpression)
     return p
+}
+
+func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
+    exp := &ast.IndexExpression{Token: p.curToken, Left: left}
+    p.nextToken()
+    exp.Index = p.parseExpression(LOWEST)
+    if !p.expectPeek(token.RBRACKET) {
+        return nil
+    }
+    return exp
 }
 
 func (p *Parser) parseArrayLiteral() ast.Expression {
@@ -297,6 +308,7 @@ const (
     PRODUCT         // *
     PREFIX          // -X or !X
     CALL            // myFunction(X)
+    INDEX           // array[index]
 )
 
 func (p *Parser) noPrefixParseFnError(t token.TokenType) {
@@ -437,6 +449,7 @@ var precedences = map[token.TokenType]int {
     token.SLASH:    PRODUCT,
     token.ASTERISK: PRODUCT,
     token.LPAREN:   CALL,
+    token.LBRACKET: INDEX,
 }
 
 func (p *Parser) peekPrecedence() int {
