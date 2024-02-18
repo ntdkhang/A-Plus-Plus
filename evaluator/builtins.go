c8ab5999948dff950fbfd0653c8060837d4a15e2
ntdkhang <ntdkhang143@gmail.com>
2024-02-18 15:57:38 -0600

    add print function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "A-Plus-Plus/object"
+import (
+	"A-Plus-Plus/object"
+	"fmt"
+)
 
 var builtins = map[string]*object.Builtin {
     "len": {
@@ -83,6 +86,14 @@ var builtins = map[string]*object.Builtin {
             newElements[length] = args[1]
 
             return &object.Array{Elements: newElements}
+        },
+    },
+    "print": &object.Builtin{
+        Fn: func(args ...object.Object) object.Object {
+            for _, arg := range args {
+                fmt.Println(arg.Inspect())
+            }
+            return NULL
         },
     },
 }
